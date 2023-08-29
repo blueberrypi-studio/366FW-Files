dee737eb29378771d136c9ab6b8bcda07dd8e502
TheDude1134 <jdykes1134@gmail.com>
2023-08-28 22:21:24 -0500

    Update OperationEspressoMartini.lua

--- MissionScripts/OperationEspressoMartini/OperationEspressoMartini.lua
+++ MissionScripts/OperationEspressoMartini/OperationEspressoMartini.lua
@@ -28,7 +28,7 @@ Blue support forces: CAP, and Helo Escorts
 
 
 local DEBUG = true
-local DEBUG_PARKING = false
+local DEBUG_PARKING = true
 
 
 
@@ -218,6 +218,51 @@ end
 
 
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- TODO REDFOR AWACS
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+local AwacsRed = AIRWING:New("WarehouseBandar", "AWACSRED")
+  AwacsRed:SetReportOn()
+  AwacsRed:SetMarker(false)
+  AwacsRed:SetAirbase(AIRBASE:FindByName(AIRBASE.PersianGulf.Bandar_Abbas_Intl))
+  AwacsRed:SetRespawnAfterDestroyed(300)
+  AwacsRed:SetTakeoffAir()
+  AwacsRed:__Start(2)
+
+-- AWACS itself
+local AwacsRedSquadron = SQUADRON:New("AWACSRED", 2, "AWACSRED")
+  AwacsRedSquadron:AddMissionCapability({AUFTRAG.Type.ORBIT},100)
+  AwacsRedSquadron:SetFuelLowRefuel(true)
+  AwacsRedSquadron:SetFuelLowThreshold(0.2)
+  AwacsRedSquadron:SetTurnoverTime(10,20)
+  AwacsRed:AddSquadron(AwacsRedSquadron)
+  AwacsRed:NewPayload("AWACSRED",-1,{AUFTRAG.Type.ORBIT},100)
+
+
+
+-- Escorts
+local AwacsEscortsRed = SQUADRON:New("✈ MIG29ESCORT",4,"✈ MIG29ESCORT")
+  AwacsEscortsRed:AddMissionCapability({AUFTRAG.Type.ESCORT})
+  AwacsEscortsRed:SetFuelLowRefuel(true)
+  AwacsEscortsRed:SetFuelLowThreshold(0.3)
+  AwacsEscortsRed:SetTurnoverTime(10,20)
+  AwacsEscortsRed:SetTakeoffAir()
+  AwacsEscortsRed:SetRadio(275,radio.modulation.AM)
+  AwacsRed:AddSquadron(AwacsEscortsRed)
+  AwacsRed:NewPayload("✈ MIG29ESCORT",-1,{AUFTRAG.Type.ESCORT},100)
+
+
+local RedAwacs = AWACS:New("Awacs-Red", AwacsRed, "red", AIRBASE.PersianGulf.Bandar_Abbas_Intl, "REDAWACSORBIT", ZONE:FindByName("FEZ"), "Qeshm_Island", 275, radio.modulation.AM )
+
+  RedAwacs:SetEscort(2)
+  RedAwacs:SetAwacsDetails(CALLSIGN.AWACS.Magic,1,30,280,88,25)
+
+  RedAwacs:__Start(5)
+
+
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- TODO HELO TRANSPORT
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -277,10 +322,7 @@ local redArmorGroupCharlie = ARMYGROUP:New("Red Armor Group Charlie")
 --[[
 
 
-YOU HAVE ALOT OF SHIT TO DO HERE.  FIX THIS.
 
-
-local redArmorMission = AUFTRAG:NewCAPTUREZONE()
 
 
 
@@ -297,55 +339,55 @@ local redArmorMission = AUFTRAG:NewCAPTUREZONE()
 --CAP SQUADRONS
 
 local redCapOne=SQUADRON:New("✈ MIG29S", 16, "✈ MIG29S") --Ops.Squadron#SQUADRON
-redCapOne:AddMissionCapability(AUFTRAG.Type.CAP)
-redCapOne:SetGrouping(2)
-redCapOne:SetModex(100)
-redCapOne:SetSkill(AI.Skill.ACE)
-redCapOne:SetDespawnAfterHolding(true)
+  redCapOne:AddMissionCapability(AUFTRAG.Type.CAP)
+  redCapOne:SetGrouping(2)
+  redCapOne:SetModex(100)
+  redCapOne:SetSkill(AI.Skill.ACE)
+  redCapOne:SetDespawnAfterHolding(true)
 
 
 local redCapTwo=SQUADRON:New("✈ SU33", 16, "✈ SU33") --Ops.Squadron#SQUADRON
-redCapTwo:AddMissionCapability(AUFTRAG.Type.CAP)
-redCapTwo:SetGrouping(2)
-redCapTwo:SetModex(200)
-redCapTwo:SetSkill(AI.Skill.ACE)
-redCapTwo:SetTakeoffHot()
-redCapTwo:SetDespawnAfterHolding(true)
+  redCapTwo:AddMissionCapability(AUFTRAG.Type.CAP)
+  redCapTwo:SetGrouping(2)
+  redCapTwo:SetModex(200)
+  redCapTwo:SetSkill(AI.Skill.ACE)
+  redCapTwo:SetTakeoffHot()
+  redCapTwo:SetDespawnAfterHolding(true)
 
 local redCapThree=SQUADRON:New("✈ MIRAGE2000", 16, "✈ MIRAGE2000") --Ops.Squadron#SQUADRON
-redCapThree:AddMissionCapability(AUFTRAG.Type.CAP)
-redCapThree:SetGrouping(2)
-redCapThree:SetModex(300)
-redCapThree:SetSkill(AI.Skill.ACE)
-redCapThree:SetTakeoffHot()
-redCapThree:SetDespawnAfterHolding(true)
+  redCapThree:AddMissionCapability(AUFTRAG.Type.CAP)
+  redCapThree:SetGrouping(2)
+  redCapThree:SetModex(300)
+  redCapThree:SetSkill(AI.Skill.ACE)
+  redCapThree:SetTakeoffHot()
+  redCapThree:SetDespawnAfterHolding(true)
 
---RED AIRWING
+--REDFOR AIRWING
 
 local redAirwingOne = AIRWING:New("WarehouseQeshm", "Mocha Frappe")
 
+  redAirwingOne:NewPayload("✈ MIG29S", 99, AUFTRAG.Type.CAP, 100)
+  redAirwingOne:NewPayload("✈ SU33", 99, AUFTRAG.Type.CAP, 100)
+  redAirwingOne:NewPayload("✈ MIRAGE2000", 99, AUFTRAG.Type.CAP, 100)
 
-redAirwingOne:NewPayload("✈ MIG29S", 99, AUFTRAG.Type.CAP, 100)
-redAirwingOne:NewPayload("✈ SU33", 99, AUFTRAG.Type.CAP, 100)
-redAirwingOne:NewPayload("✈ MIRAGE2000", 99, AUFTRAG.Type.CAP, 100)
-
-redAirwingOne:AddSquadron(redCapOne)
-redAirwingOne:AddSquadron(redCapTwo)
-redAirwingOne:AddSquadron(redCapThree)
-
+  redAirwingOne:AddSquadron(redCapOne)
+  redAirwingOne:AddSquadron(redCapTwo)
+  redAirwingOne:AddSquadron(redCapThree)
 
 
+--REDFOR CAP MISSION
 
 local missionRedCAPzone = AUFTRAG:NewCAP(redCAPzone, 15000, 350, nil, 90, 20, {"Air"})
-missionRedCAPzone:SetRequiredAssets(2)
-missionRedCAPzone:SetRepeatOnFailure(16)
-missionRedCAPzone:SetROE(ENUMS.ROE.OpenFire)
+  missionRedCAPzone:SetRequiredAssets(2)
+  missionRedCAPzone:SetRepeatOnFailure(16)
+  missionRedCAPzone:SetROE(ENUMS.ROE.OpenFire)
 
-redAirwingOne:AddMission(missionRedCAPzone)
 
-redAirwingOne:AddPatrolPointCAP(ZONE:New("REDCAPZONE"):GetCoordinate(),math.random(15000, 25000),math.random(300, 450),math.random(1,360),math.random(20, 50))
-redAirwingOne:SetTakeoffAir()
-redAirwingOne:SetNumberCAP(2)
+  redAirwingOne:AddMission(missionRedCAPzone)
+
+  redAirwingOne:AddPatrolPointCAP(ZONE:New("REDCAPZONE"):GetCoordinate(),math.random(15000, 25000),math.random(300, 450),math.random(1,360),math.random(20, 50))
+  redAirwingOne:SetTakeoffAir()
+  redAirwingOne:SetNumberCAP(2)
 
 
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -357,3 +399,160 @@ local redforCommander = COMMANDER:New(coalition.side.RED)
   redforCommander:AddAirwing(redAirwingOne)
 
   redforCommander:__Start(3)
+  
+  
+  
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- TODO BLUE TANKER
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+local ZoneBoom=ZONE:FindByName("Zone Boom")
+local ZoneProbe=ZONE:FindByName("Zone Probe")
+local ZonePointBoom=ZONE:FindByName("Point Boom")
+local ZonePointProbe=ZONE:FindByName("Point Probe")
+
+-- KC-135 squadron, callsign "Arco".
+local kc135=SQUADRON:New("✈ KC135", 5, "✈ KC135") --Ops.Squadron#SQUADRON
+  kc135:SetModex(100)
+  kc135:SetCallsign(CALLSIGN.Tanker.Arco)
+  kc135:SetRadio(260)
+  kc135:SetSkill(AI.Skill.EXCELLENT)
+  kc135:AddMissionCapability({AUFTRAG.Type.TANKER}, 100)
+  kc135:AddTacanChannel(70, 75)
+
+-- KC-130 squadron, callsign "Texaco".
+local kc130=SQUADRON:New("✈ KC130", 5, "✈ KC130") --Ops.Squadron#SQUADRON
+  kc130:SetModex(200)
+  kc130:SetCallsign(CALLSIGN.Tanker.Texaco)
+  kc130:SetRadio(261)
+  kc130:SetSkill(AI.Skill.EXCELLENT)
+  kc130:AddMissionCapability({AUFTRAG.Type.TANKER}, 100)
+  kc130:AddTacanChannel(80, 85)
+
+
+local blueTankerWing=AIRWING:New("WarehouseSharjah", "Need Gas?") --Ops.AirWing#AIRWING
+  blueTankerWing:AddPatrolPointTANKER(ZonePointBoom, 20000, 350, 180, 30, Unit.RefuelingSystem.BOOM_AND_RECEPTACLE)
+  blueTankerWing:AddPatrolPointTANKER(ZonePointProbe, 20000, 350, 0, 30, Unit.RefuelingSystem.PROBE_AND_DROGUE)
+  blueTankerWing:SetNumberTankerBoom(1)
+  blueTankerWing:SetNumberTankerProbe(1)
+  blueTankerWing:AddSquadron(kc135)
+  blueTankerWing:AddSquadron(kc130)
+  blueTankerWing:SetTakeoffAir()
+
+
+
+  blueTankerWing:Start()
+
+local function NewTankerMission(RefuelSystem)
+
+  local Coordinate
+  if RefuelSystem==Unit.RefuelingSystem.BOOM_AND_RECEPTACLE then
+    Coordinate=ZoneBoom:GetCoordinate()
+  elseif RefuelSystem==Unit.RefuelingSystem.PROBE_AND_DROGUE then
+    Coordinate=ZoneProbe:GetCoordinate()
+  end
+
+  local mission=AUFTRAG:NewTANKER(Coordinate, 20000, 350, 000, 25, RefuelSystem)
+
+  blueTankerWing:AddMission(mission)
+  
+
+  function mission:OnAfterDone()
+    MESSAGE:New(string.format("Mission %s done. Creating new TANKER mission for refuel system type %d", mission:GetName(), RefuelSystem), 360):ToAll():ToLog()
+    NewTankerMission(RefuelSystem)
+  end
+  
+end
+
+NewTankerMission(Unit.RefuelingSystem.BOOM_AND_RECEPTACLE)
+
+NewTankerMission(Unit.RefuelingSystem.PROBE_AND_DROGUE)
+
+
+--- Function called each time a flight group goes on a mission. Can be used to fine tune.
+function blueTankerWing:OnAfterFlightOnMission(From, Event, To, Flightgroup, Mission)
+  local flightgroup=Flightgroup --Ops.FlightGroup#FLIGHTGROUP
+  local mission=Mission --Ops.Auftrag#AUFTRAG
+  
+
+  local tacanChannel, tacanMorse, tacanBand=flightgroup:GetTACAN()
+  
+  local text=string.format("Flight %s on %s mission %s. TACAN Channel %s%s (%s)", flightgroup:GetName(), mission:GetType(), mission:GetName(), tostring(tacanChannel), tostring(tacanBand), tostring(tacanMorse))
+  MESSAGE:New(text):ToAll():ToLog()  
+  
+  
+  flightgroup:SetFuelLowThreshold(25)
+end  
+  
+
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- TODO ZONE CAPTURE
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+local CaptureZoneAlpha = ZONE_AIRBASE:New(AIRBASE.PersianGulf.Khasab, 10000)
+
+local ZoneCaptureCoalitionOne = ZONE_CAPTURE_COALITION:New( CaptureZoneAlpha, coalition.side.RED ) 
+
+
+--- @param Functional.ZoneCaptureCoalition#ZONE_CAPTURE_COALITION self
+function ZoneCaptureCoalitionOne:OnEnterGuarded( From, Event, To )
+  if From ~= To then
+    local Coalition = self:GetCoalition()
+    self:E( { Coalition = Coalition } )
+    if Coalition == coalition.side.BLUE then
+      ZoneCaptureCoalitionOne:Smoke( SMOKECOLOR.Blue )
+      US_CC:MessageTypeToCoalition( string.format( "%s is under protection of the USA", ZoneCaptureCoalitionOne:GetZoneName() ), MESSAGE.Type.Information )
+      RU_CC:MessageTypeToCoalition( string.format( "%s is under protection of the USA", ZoneCaptureCoalitionOne:GetZoneName() ), MESSAGE.Type.Information )
+    else
+      ZoneCaptureCoalitionOne:Smoke( SMOKECOLOR.Red )
+      RU_CC:MessageTypeToCoalition( string.format( "%s is under protection of Russia", ZoneCaptureCoalitionOne:GetZoneName() ), MESSAGE.Type.Information )
+      US_CC:MessageTypeToCoalition( string.format( "%s is under protection of Russia", ZoneCaptureCoalitionOne:GetZoneName() ), MESSAGE.Type.Information )
+    end
+  end
+end
+
+--- @param Functional.Protect#ZONE_CAPTURE_COALITION self
+function ZoneCaptureCoalitionOne:OnEnterEmpty()
+  ZoneCaptureCoalitionOne:Smoke( SMOKECOLOR.Green )
+  --Start Blue Bravo Brigade
+  US_CC:MessageTypeToCoalition( string.format( "%s is unprotected, and can be captured!", ZoneCaptureCoalitionOne:GetZoneName() ), MESSAGE.Type.Information )
+  RU_CC:MessageTypeToCoalition( string.format( "%s is unprotected, and can be captured!", ZoneCaptureCoalitionOne:GetZoneName() ), MESSAGE.Type.Information )
+end
+
+
+--- @param Functional.Protect#ZONE_CAPTURE_COALITION self
+function ZoneCaptureCoalitionOne:OnEnterAttacked()
+  ZoneCaptureCoalitionOne:Smoke( SMOKECOLOR.White )
+  local Coalition = self:GetCoalition()
+  self:E({Coalition = Coalition})
+  if Coalition == coalition.side.BLUE then
+    US_CC:MessageTypeToCoalition( string.format( "%s is under attack by Russia", ZoneCaptureCoalitionOne:GetZoneName() ), MESSAGE.Type.Information )
+    RU_CC:MessageTypeToCoalition( string.format( "We are attacking %s", ZoneCaptureCoalitionOne:GetZoneName() ), MESSAGE.Type.Information )
+  else
+    RU_CC:MessageTypeToCoalition( string.format( "%s is under attack by the USA", ZoneCaptureCoalitionOne:GetZoneName() ), MESSAGE.Type.Information )
+    US_CC:MessageTypeToCoalition( string.format( "We are attacking %s", ZoneCaptureCoalitionOne:GetZoneName() ), MESSAGE.Type.Information )
+  end
+end
+
+--- @param Functional.Protect#ZONE_CAPTURE_COALITION self
+function ZoneCaptureCoalitionOne:OnEnterCaptured()
+  local Coalition = self:GetCoalition()
+  self:E({Coalition = Coalition})
+  if Coalition == coalition.side.BLUE then
+    RU_CC:MessageTypeToCoalition( string.format( "%s is captured by the USA, we lost it!", ZoneCaptureCoalitionOne:GetZoneName() ), MESSAGE.Type.Information )
+    US_CC:MessageTypeToCoalition( string.format( "We captured %s, Excellent job!", ZoneCaptureCoalitionOne:GetZoneName() ), MESSAGE.Type.Information )
+  else
+    US_CC:MessageTypeToCoalition( string.format( "%s is captured by Russia, we lost it!", ZoneCaptureCoalitionOne:GetZoneName() ), MESSAGE.Type.Information )
+    RU_CC:MessageTypeToCoalition( string.format( "We captured %s, Excellent job!", ZoneCaptureCoalitionOne:GetZoneName() ), MESSAGE.Type.Information )
+  end
+  
+  self:AddScore( "Captured", "Zone captured: Extra points granted.", 200 )    
+  
+  self:__Guard( 30 )
+end
+
+ZoneCaptureCoalitionOne:__Guard( 1 )
+  
+ZoneCaptureCoalitionOne:Start( 30, 120 )
